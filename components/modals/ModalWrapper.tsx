d90edb51d94f83c5ead9de9e34e56febcb303c99
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-02-18 12:06:07 -0700

    fix modal margins

--- components/modals/ModalWrapper.tsx
+++ components/modals/ModalWrapper.tsx
@@ -34,35 +34,26 @@ const ModalWrapper = ({ id, children, showModal, setShowModal }: IModalWrapper) 
     return () => document.removeEventListener("keydown", keyHandler)
   })
   return (
-    <Transition show={showModal}>
-      <div className="fixed z-10 inset-0 top-12 -bottom-12 overflow-y-auto">
-        <div className="flex items-start justify-center min-h-screen text-center sm:block sm:p-0">
-          <Transition.Child
-            className="fixed inset-0 transition-opacity overflow-visible"
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="absolute z-10 inset-0 bg-gray-500 opacity-75"></div>
-            <Icon name="close" className="z-20 absolute right-2 top-2 text-white h-8 w-8 cursor-pointer " />
-          </Transition.Child>
-          <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-            &#8203;
-          </span>
+    <Transition show={showModal} className="fixed z-10 inset-0">
+      <Transition.Child
+        className="fixed inset-0 transition-opacity"
+        enter="ease-out duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="ease-in duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="absolute z-10 inset-0 bg-gray-600 opacity-75"></div>
+        <Icon name="close" className="absolute z-20 right-2 top-2 text-white h-8 w-8 cursor-pointer" />
+      </Transition.Child>
 
-          <div
-            ref={modalContent}
-            className="z-20 inline-block align-bottom sm:align-middle sm:max-w-xl sm:w-full"
-            role="dialog"
-            aria-modal="true"
-            aria-labelledby="modal-headline"
-          >
+      <div className="z-20 flex justify-center h-screen w-screen overflow-y-auto mt-12">
+        <div ref={modalContent} className="sm:m-auto sm:max-w-xl sm:w-full">
+          <div className="pb-12">
             <Transition.Child
               id={id}
-              className="transform transition-all bg-white text-left overflow-hidden shadow-xl mb-12 sm:rounded-lg"
+              className="transform transition-all bg-white text-left shadow-xl sm:overflow-hidden sm:rounded-lg"
               enter="ease-out duration-300"
               enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               enterTo="opacity-100 translate-y-0 sm:scale-100"
