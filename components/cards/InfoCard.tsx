3ccf89384bd352366047131054044f7da536bf12
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-02-21 08:24:07 -0700

    fix nested button

--- components/cards/InfoCard.tsx
+++ components/cards/InfoCard.tsx
@@ -27,10 +27,10 @@ const InfoCard = ({ ...card }: IInfoCard) => {
           </p>
         </div>
         <div className="flex-shrink-0 pr-2">
-          <button className="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <div className="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             <span className="sr-only">Open options</span>
             <Icon name="chevron-right" className="w-5 h-5" />
-          </button>
+          </div>
         </div>
       </div>
     </PrimaryButton>
