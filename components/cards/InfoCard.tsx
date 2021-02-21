d02d1a6b96ef95fcf2a749dd2b2fd9946437e413
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-02-21 08:44:20 -0700

    add images to announcements

--- components/cards/InfoCard.tsx
+++ components/cards/InfoCard.tsx
@@ -1,9 +1,10 @@
 import PrimaryButton from "../buttons/PrimaryButton"
 import Icon from "../shared/Icon"
-import { IButton } from "../shared/Interfaces"
+import { IButton, IImage } from "../shared/Interfaces"
 
 export type IInfoCard = {
   title: string
+  image?: IImage
   button?: IButton
   hidden?: boolean
 }
@@ -16,7 +17,13 @@ const InfoCard = ({ ...card }: IInfoCard) => {
       modal={card.button.modal}
       className="flex text-left col-span-1 shadow-sm rounded-md focus:outline-none"
     >
-      <div className="flex-shrink-0 flex items-center justify-center w-16 bg-green-600 text-white text-sm font-medium rounded-l-md" />
+      <div className="flex-shrink-0 flex w-16 rounded-l-md">
+        <img
+          className="w-full h-full object-cover rounded-t-lg md:rounded-l-lg md:rounded-t-none"
+          src={card.image?.src || "https://via.placeholder.com/382"}
+          alt={card.image?.alt || ""}
+        />
+      </div>
       <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
         <div className="flex-1 px-4 py-2 text-sm overflow-hidden">
           <div className="text-gray-900 font-medium truncate hover:text-gray-600" title={card.title}>
@@ -27,7 +34,7 @@ const InfoCard = ({ ...card }: IInfoCard) => {
           </p>
         </div>
         <div className="flex-shrink-0 pr-2">
-          <div className="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <div className="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 bg-transparent hover:text-gray-500">
             <span className="sr-only">Open options</span>
             <Icon name="chevron-right" className="w-5 h-5" />
           </div>
