e1a71875478947328ccfd7d0a033c9eca85e7f4b
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-02-22 02:25:12 -0700

    fix info card

--- components/cards/InfoCard.tsx
+++ components/cards/InfoCard.tsx
@@ -18,41 +18,42 @@ const InfoCard = ({ ...card }: IInfoCard) => {
       type="pass-thru"
       link={card.button.link}
       modal={card.button.modal}
-      className="flex text-left h-full col-span-1 shadow-xl rounded-md bg-white focus:outline-none"
+      className="relative rounded-lg shadow-xl bg-white px-4 py-5 flex text-left items-center space-x-3"
     >
-      <div className="w-16 md:w-20">
-        <img className="w-full h-full object-cover rounded-l-md" src={card.image?.src || "https://via.placeholder.com/382"} alt={card.image?.alt || ""} />
+      <div className="flex-shrink-0">
+        <img className="h-16 w-16 rounded-full border border-gray-400" src={card.image?.src || "https://via.placeholder.com/382"} alt={card.image?.alt || ""} />
       </div>
-      <div className="flex-1 flex items-center justify-between truncate">
-        <div className="flex-1 px-4 py-2 text-sm overflow-hidden">
-          <div className="text-black font-semibold text-md pb-1 md:text-lg truncate" title={card.title}>
+      <div className="flex-1 min-w-0">
+        <a href="#" className="focus:outline-none">
+          <span className="absolute inset-0" aria-hidden="true"></span>
+          <p className="text-black font-semibold text-md md:text-lg pb-1 truncate" title={card.title}>
             {card.title}
-          </div>
+          </p>
           {card.date && (
-            <div className="text-gray-500 flex flex-wrap pb-1">
+            <div className="text-sm md:text-md text-gray-600 flex flex-wrap leading-6">
               <div className="pr-2 flex items-center">
-                <Icon name="calendar" className="h-4 w-4 mr-2 text-gray-300" />
+                <Icon name="calendar" className="h-4 w-4 mr-2 mb-0.5 text-gray-400" />
                 <time dateTime={dateDisplay.isoString}>{dateDisplay.dateDisplay}</time>
               </div>
               <div className="pr-2 flex items-center">
-                <Icon name="clock" className="h-4 w-4 mr-2 text-gray-300" />
+                <Icon name="clock" className="h-4 w-4 mr-2 mb-0.5 text-gray-400" />
                 <time dateTime={dateDisplay.isoString}>{dateDisplay.timeDisplay}</time>
-              </div>
-              <div
-                className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${
-                  dateDisplay.isWithin ? "bg-green-100 text-green-800" : "bg-primary-100 text-primary-800"
-                }`}
-              >
-                {dateDisplay.relativeDisplay}
+                <span
+                  className={`inline-flex items-center ml-2 px-2.5 py-0.5 rounded-full text-xs font-medium ${
+                    dateDisplay.isWithin ? "bg-green-100 text-green-800" : "bg-primary-100 text-primary-800"
+                  }`}
+                >
+                  {dateDisplay.relativeDisplay}
+                </span>
               </div>
             </div>
           )}
-        </div>
-        <div className="flex-shrink-0 pr-2">
-          <div className="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 bg-transparent hover:text-gray-500">
-            <span className="sr-only">Open options</span>
-            <Icon name="chevron-right" className="w-5 h-5" />
-          </div>
+        </a>
+      </div>
+      <div className="flex-shrink-0 pr-2">
+        <div className="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 bg-transparent hover:text-gray-500">
+          <span className="sr-only">Open options</span>
+          <Icon name="chevron-right" className="w-5 h-5" />
         </div>
       </div>
     </PrimaryButton>
--- components/headers/SectionHeader.tsx
+++ components/headers/SectionHeader.tsx
@@ -6,7 +6,7 @@ export type ISectionHeader = {
 
 const SectionHeader = ({ title, subtitle = "", className = "" }: ISectionHeader) => {
   return (
-    <div className={`${className} pt-10 sm:pt-14 lg:pt-20 max-w-7xl mx-auto text-center md:text-left`}>
+    <div className={`${className} pt-10 sm:pt-14 lg:pt-20 max-w-7xl mx-auto text-center`}>
       <div className="space-y-8 sm:space-y-12">
         <div className="space-y-2 sm:space-y-4">
           <h2 className="text-xl font-extrabold tracking-tight sm:text-3xl md:text-4xl">{title}</h2>
