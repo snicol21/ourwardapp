ef30d595e7a5273d5636058f14871bc883591d64
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-02-23 17:32:57 -0700

    remove z-indexes

--- components/modules/modals/ModalEvent/ModalEventAction/ModalEventAction.tsx
+++ components/modules/modals/ModalEvent/ModalEventAction/ModalEventAction.tsx
@@ -8,13 +8,13 @@ const ModalEventAction = () => {
   return (
     <div>
       <div className="block">
-        <nav className="relative z-0 rounded-lg shadow flex divide-x divide-gray-200" aria-label="Tabs">
+        <nav className="relative rounded-lg shadow flex divide-x divide-gray-200" aria-label="Tabs">
           <button
             onClick={() => setAction("add")}
             aria-current="page"
             className={`${
               action === "add" ? "text-primary font-semibold" : "text-gray-500 font-medium hover:text-gray-700"
-            } rounded-l-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-md text-center hover:bg-gray-50 focus:z-10 focus:outline-none`}
+            } rounded-l-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-md text-center hover:bg-gray-50 focus:outline-none`}
           >
             <span>Add</span>
             <span aria-hidden="true" className={`${action === "add" ? "bg-primary-500" : "bg-transparent"} absolute inset-x-0 bottom-0 h-0.5`}></span>
@@ -24,7 +24,7 @@ const ModalEventAction = () => {
             aria-current="false"
             className={`${
               action === "update" ? "text-primary font-semibold" : "text-gray-500 font-medium hover:text-gray-700"
-            } group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center hover:bg-gray-50 focus:z-10 focus:outline-none`}
+            } group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center hover:bg-gray-50 focus:outline-none`}
           >
             <span>Update</span>
             <span aria-hidden="true" className={`${action === "update" ? "bg-primary-500" : "bg-transparent"} absolute inset-x-0 bottom-0 h-0.5`}></span>
