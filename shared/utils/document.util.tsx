33c0f70ef1dc0646fed16431458961a0b71b5152
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-02-23 20:15:23 -0700

    update the hide scrollbar to just apply a class instead

--- shared/utils/document.util.tsx
+++ shared/utils/document.util.tsx
@@ -1,6 +1,5 @@
 export const hideBodyScroll = (hide: boolean) => {
   if (typeof document !== `undefined`) {
-    const setOverFlowY = (value) => (document.getElementsByTagName("body")[0].style.overflowY = value)
-    hide ? setOverFlowY("hidden") : setOverFlowY("auto")
+    hide ? document.body.classList.add("is-scrollLocked") : document.body.classList.remove("is-scrollLocked")
   }
 }
