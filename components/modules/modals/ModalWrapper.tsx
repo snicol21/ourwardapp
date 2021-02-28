802ce05643490b2bcafb285fb8933925c802f893
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-02-27 17:36:22 -0700

    revert changes

--- components/modules/modals/ModalWrapper.tsx
+++ components/modules/modals/ModalWrapper.tsx
@@ -2,6 +2,7 @@ import { cloneElement, Dispatch, SetStateAction } from "react"
 import { Transition } from "@headlessui/react"
 import { useEffect, useRef } from "react"
 import Icon from "../../elements/icons/Icon"
+import { hideBodyScroll } from "../../../shared/utils/document.util"
 import React from "react"
 
 export type IModalWrapper = {
@@ -14,9 +15,10 @@ export type IModalWrapper = {
 }
 
 const ModalWrapper = ({ id, children, closeOnClickOutside, showModal, setShowModal, className = "" }: IModalWrapper) => {
-  const targetElement = useRef(null)
+  const modalContent = useRef(null)
 
   const toggleModal = () => {
+    hideBodyScroll(!showModal)
     setShowModal(!showModal)
   }
 
@@ -25,7 +27,7 @@ const ModalWrapper = ({ id, children, closeOnClickOutside, showModal, setShowMod
       return
     }
     const clickHandler = ({ target }) => {
-      if (!showModal || targetElement.current.contains(target)) {
+      if (!showModal || modalContent.current.contains(target)) {
         return
       }
       toggleModal()
@@ -63,7 +65,7 @@ const ModalWrapper = ({ id, children, closeOnClickOutside, showModal, setShowMod
       </Transition.Child>
 
       <div className="z-20 flex justify-center h-screen w-screen overflow-y-auto mt-12">
-        <div ref={targetElement} className={`${className}`}>
+        <div ref={modalContent} className={`${className}`}>
           <div className="pb-32">
             <Transition.Child
               id={id}
