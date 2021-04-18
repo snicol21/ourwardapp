36b9c80c7414e0159a94569dd95f819d9b329dc3
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-04-18 09:45:46 -0600

    add special youth devotion and fix cleaning image so bishop doesn't make fun of me

--- data/dataIndex.tsx
+++ data/dataIndex.tsx
@@ -23,36 +23,6 @@ export const dataSundayMeeting: IHeroCard = {
 
 export const dataInfoCards: IEventCard[] = [
   {
-    title: "Relief Society Activity",
-    date: new Date("April 13, 2021 19:00:00"),
-    image: {
-      src: "/images/events/relief-society.png",
-    },
-    button: {
-      modal: {
-        type: "event",
-        data: {
-          title: "Relief Society Activity",
-          subtitle: "All Adult Women",
-          image: {
-            src: "/images/events/relief-society.png",
-          },
-          date: new Date("April 13, 2021 19:00:00"),
-          duration: 60,
-          location: "Zoom",
-          button: {
-            text: "Join Zoom",
-            link: {
-              url: "http://virtual-rs.ourwardapp.com",
-              external: true,
-            },
-          },
-        },
-      },
-    },
-    hidden: false,
-  },
-  {
     title: "Bishop Mobile",
     date: new Date("April 15, 2021 17:00:00"),
     image: {
@@ -75,62 +45,7 @@ export const dataInfoCards: IEventCard[] = [
         },
       },
     },
-    hidden: false,
-  },
-  {
-    title: "Welfare Assignment",
-    date: new Date("April 14, 2021 16:30:00"),
-    image: {
-      src: "/images/events/welfare.png",
-    },
-    button: {
-      modal: {
-        type: "event",
-        data: {
-          title: "Welfare Assignment",
-          image: {
-            src: "/images/events/welfare.png",
-          },
-          date: new Date("April 14, 2021 16:30:00"),
-          duration: 120,
-          location: "242 West Paramount Avenue, Salt Lake City UT, 84115 - Gail Miller Homeless Resource Center",
-          details:
-            "Request:\nThree Volunteers\nWednesday, April 14\n4:30 to 6:30 PM\n\nAssignment Details:\nIf you are experiencing COVID symptoms, please stay home, because you will be working around food, please make sure that you are showered and that your clothes are clean\n\nPlease be sure to bring the following items:\n1- Close toed shoes and socks\n2- A shirt with sleeves. The shirt can be long or short sleeved- just no tanktops\n3- A hair tie - for hair that is longer than five inches\n4- A hat - a hair net can be worn instead if you prefer that. Hair nets, beard nets, aprons, and gloves will be provided upon arrival\n\nWhere to park?\nFrom our main entrance you will see a gated area. Since it is gated you will need to use the call box to enter. When you arrive, simply press the call button and let them know that you are here to volunteer. One of our staff will open the gate for you from inside. Once in the parking area, you will follow the road around the building until you see some parking spots. Feel free to park in any open spot.\n\nHow to get inside?\nPlease enter through the front main entrance of the building.\n\nPlease contact Bro. Jason Hardman to sign up or if you have any questions at 801-613-9833",
-        },
-      },
-    },
-    hidden: false,
-  },
-  {
-    title: "Building Cleaning",
-    date: new Date("April 17, 2021 10:00:00"),
-    image: {
-      src: "/images/events/cleaning.png",
-    },
-    button: {
-      modal: {
-        type: "event",
-        data: {
-          title: "Building Cleaning",
-          image: {
-            src: "/images/events/cleaning.png",
-          },
-          date: new Date("April 17, 2021 10:00:00"),
-          duration: 60,
-          location: "Ward Building",
-          details:
-            "We have the opportunity to clean the building. Please sign up and bring your family to help clean the building. Supplies will be provided upon arrival.",
-          button: {
-            text: "Sign Up",
-            link: {
-              url: "https://calendly.com/ssr3rdward/cleaning",
-              external: true,
-            },
-          },
-        },
-      },
-    },
-    hidden: false,
+    hidden: true,
   },
   {
     title: "Building Cleaning",
@@ -164,6 +79,36 @@ export const dataInfoCards: IEventCard[] = [
     hidden: false,
   },
   {
+    title: "Special Youth Devotional",
+    date: new Date("April 24, 2021 17:00:00"),
+    image: {
+      src: "/images/events/fireside.png",
+    },
+    button: {
+      modal: {
+        type: "event",
+        data: {
+          title: "Special Youth Devotional",
+          image: {
+            src: "/images/events/fireside.png",
+          },
+          date: new Date("April 24, 2021 17:00:00"),
+          duration: 120,
+          location: "Virtual Event",
+          details: "Speakers will be Elder Rasband, Elder Schmutz, Bishop Causse of the Presiding Bishopric, Elder Kahnlein, and Sis. Eubanks.",
+          button: {
+            text: "Broadcast",
+            link: {
+              url: "https://churchofjesuschrist.org/broadcasts/languages/west-jordan-west-devotional/2021/04",
+              external: true,
+            },
+          },
+        },
+      },
+    },
+    hidden: false,
+  },
+  {
     title: "Special Ward Activity",
     date: new Date("April 25, 2021 19:00:00"),
     image: {
@@ -183,30 +128,6 @@ export const dataInfoCards: IEventCard[] = [
           location: "Oaks Building",
           details:
             "We will have a special ward activity on Sunday, April 25th at 7pm for all adults. It will be held in person at the Oaks building (where we meet). We invite all to attend.\n\nIn preparation for this activity, please pay particular attention to General Conference this weekend. Take note of the things that inspire you and come prepared to share your experience and feelings with others.\n\nThis will be an unique and enjoyable evening of inspiration and fellowship. Please plan to attend.",
-        },
-      },
-    },
-    hidden: false,
-  },
-  {
-    title: "Combined Youth Activity",
-    date: new Date("April 14, 2021 19:00:00"),
-    image: {
-      src: "/images/events/combined-youth.png",
-    },
-    button: {
-      modal: {
-        type: "event",
-        data: {
-          title: "Combined Youth Activity",
-          subtitle: "For All Youth",
-          image: {
-            src: "/images/events/combined-youth.png",
-          },
-          date: new Date("April 14, 2021 19:00:00"),
-          duration: 60,
-          location: "Ward Building",
-          details: "Combined Youth Activity with Patriarch Harris. Sunday dress is appropriate, amazing refreshments will be provided.",
         },
       },
     },
