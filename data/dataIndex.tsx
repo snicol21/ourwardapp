44b7346aa7fa679af5f03abe073a08869d24bbf9
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-06-01 14:56:24 -0600

    upload relief society activity

--- data/dataIndex.tsx
+++ data/dataIndex.tsx
@@ -105,6 +105,31 @@ export const dataInfoCards: IEventCard[] = [
     hidden: false,
   },
   {
+    title: "Relief Society Activity",
+    date: new Date("June 8, 2021 19:00:00"),
+    image: {
+      src: "/images/events/rs-summer-time.jpg",
+    },
+    button: {
+      modal: {
+        type: "event",
+        data: {
+          title: "Relief Society Activity",
+          subtitle: "All Relief Society",
+          image: {
+            src: "/images/events/rs-summer-time.jpg",
+          },
+          date: new Date("June 8, 2021 19:00:00"),
+          duration: 90,
+          location: "At the Church",
+          details:
+            "We are going to be sharing ideas for fun summer activities and having a professional come and demonstrate how to pick and cut fresh fruit . Fruit with yummy dips, sweet bread, and lemonade will  be served!",
+        },
+      },
+    },
+    hidden: false,
+  },
+  {
     title: "Relief Society Ministering Interviews",
     date: new Date("June 13, 2021 14:30:00"),
     image: {
