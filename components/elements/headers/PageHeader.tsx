4906542616056eef8c5bc1e0d8d81906da8ae029
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-07-01 16:38:17 -0600

    make the default page header subtitle text a little larger

--- components/elements/headers/PageHeader.tsx
+++ components/elements/headers/PageHeader.tsx
@@ -10,7 +10,7 @@ const PageHeader = ({ title, subtitle = "", className = "" }: IPageHeader) => {
       <div className="p-6 sm:p-12 md:p-14 lg:p-16 xl:px-28">
         <div className="w-full px-4 text-center md:px-10 lg:px-16 xl:px-24">
           <h2 className="text-xl font-extrabold text-white sm:text-4xl">{title}</h2>
-          {subtitle && <h4 className="mt-2 md:mt-4 leading-6 text-white">{subtitle}</h4>}
+          {subtitle && <h4 className="text-lg mt-2 md:mt-4 leading-6 text-white sm:text-xl">{subtitle}</h4>}
         </div>
       </div>
     </div>
