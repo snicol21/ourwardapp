ce738223f0a73cd568211c384728c0bdef101c82
HQCATALYST\spencer.nicol <spencer.nicol@healthcatalyst.com>
2021-07-01 18:10:47 -0600

    remove link references

--- components/modules/cards/ImageCard.tsx
+++ components/modules/cards/ImageCard.tsx
@@ -21,10 +21,10 @@ const ImageCard = ({ ...card }: IImageCard) => {
         <div className="flex-1 bg-white p-6 flex flex-col justify-between">
           <div className="flex-1">
             <div className="text-primary font-semibold">{card.subtitle}</div>
-            <a href="#" className="block mt-2">
+            <div className="block mt-2">
               <p className="text-xl font-semibold text-gray-900">{card.title}</p>
               {card.paragraph && <p className="mt-3 text-base text-gray-500">{card.paragraph}</p>}
-            </a>
+            </div>
           </div>
           {card.button && (
             <div className="pt-6 text-center md:text-left">
