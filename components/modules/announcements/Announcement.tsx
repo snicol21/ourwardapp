0025024b532bbd71bb19a5fb117aab7ddc6c7ae7
snicol21 <Spencer.Nicol@gmail.com>
2022-01-23 18:17:10 -0700

    allow an announcement to have multiple dates with different subtitles; for things like stake conference and general conference

--- components/modules/announcements/Announcement.tsx
+++ components/modules/announcements/Announcement.tsx
@@ -1,12 +1,18 @@
 import PrimaryButton from "../../elements/buttons/PrimaryButton"
-import { getDateDisplay } from "../../../shared/utils/date.util"
+import { getDateDisplay, IDateDisplay } from "../../../shared/utils/date.util"
 import { IButton } from "../../../shared/types"
 import Linkify from "linkify-react"
 import Icon from "../../elements/icons/Icon"
 
+type SubDate = {
+  subTitle?: string
+  date: Date
+}
+
 export type IAnnouncement = {
-  date?: Date
   title: string
+  date?: Date
+  dates?: SubDate[]
   description?: string
   button?: IButton
 }
@@ -27,27 +33,38 @@ const options = {
   validate: true,
 }
 
+const renderDate = (date: Date, subTitle?: string) => {
+  const dateDisplay = getDateDisplay(date, 7)
+  return (
+    <div className="mt-2 text-xs">
+      {subTitle && <span className="mr-2 font-semibold text-black">{subTitle}</span>}
+      <div key={dateDisplay.isoString} className="flex flex-wrap text-gray-500">
+        <div className="pr-2 flex items-center">
+          <Icon name="calendar" className="h-3 w-3 mr-1" />
+          <time dateTime={dateDisplay.isoString}>{dateDisplay.dateDisplay}</time>
+        </div>
+        <div className="pr-2 flex items-center">
+          <Icon name="clock" className="h-3 w-3 mr-1" />
+          <time dateTime={dateDisplay.isoString}>{dateDisplay.timeDisplay}</time>
+        </div>
+        {dateDisplay.isWithin && (
+          <span className="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
+            <small>{dateDisplay.relativeDisplay}</small>
+          </span>
+        )}
+      </div>
+    </div>
+  )
+}
+const renderSubDate = (subDate: SubDate) => renderDate(subDate.date, subDate.subTitle)
+
 const Announcement = ({ ...announcement }: IAnnouncement) => {
-  const dateDisplay = getDateDisplay(announcement.date, 7)
   return (
     <div key={announcement.title}>
       <div className="block">
         <div className="text-base font-semibold text-black">{announcement.title}</div>
-        <div className="mt-2 text-xs text-gray-500 flex flex-wrap">
-          <div className="pr-2 flex items-center">
-            <Icon name="calendar" className="h-3 w-3 mr-1" />
-            <time dateTime={dateDisplay.isoString}>{dateDisplay.dateDisplay}</time>
-          </div>
-          <div className="pr-2 flex items-center">
-            <Icon name="clock" className="h-3 w-3 mr-1" />
-            <time dateTime={dateDisplay.isoString}>{dateDisplay.timeDisplay}</time>
-          </div>
-          {dateDisplay.isWithin && (
-            <span className="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
-              <small>{dateDisplay.relativeDisplay}</small>
-            </span>
-          )}
-        </div>
+        {announcement.date && !announcement.dates && renderDate(announcement.date)}
+        {announcement.dates && announcement.dates.map((subDate) => renderSubDate(subDate))}
         {announcement.description && (
           <Linkify tagName="p" options={options} className="mt-2 text-sm text-gray-500">
             {announcement.description}
