b574d65282cb4a553ab018418b56cf753da06cb2
Spencer Nicol <Spencer.Nicol@gmail.com>
2022-01-30 08:55:17 -0700

    Update dataIndex.tsx

--- data/dataIndex.tsx
+++ data/dataIndex.tsx
@@ -58,7 +58,7 @@ export const dataAnnouncements: IAnnouncement[] = [
   {
     title: "Young Men's Stake Basketball",
     date: new Date("February 12, 2022 8:00:00"),
-    description: "We are putting together baskteball teams for Young Men's basketball. Games will begin on Saturday, February 12th. Have your young men voice their interest in their quorum groups."
+    description: "Young Men's Stake Basketball will begin Saturday, February 12th at 8:00 AM. Each young man wanting to participate will need a permission slip that can be found on the church website."
   },
   {
     title: "Self-Reliance Kick-off Devotional",
