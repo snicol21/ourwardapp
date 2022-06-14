64b86dca9b1d8160319d43e67099f642a2157f69
Spencer Nicol <Spencer.Nicol@gmail.com>
2022-06-14 09:18:38 -0600

    Update dataSchedules.tsx

--- data/dataSchedules.tsx
+++ data/dataSchedules.tsx
@@ -23,7 +23,7 @@ export const dataSundayScheduleTimes: IScheduleTime[] = [
         button: {
           text: "Join Live Stream",
           link: {
-            url: "https://youtu.be/0CYhm3jJqSE",
+            url: "https://youtu.be/9-axkir4DUo",
             external: true,
           },
         },
