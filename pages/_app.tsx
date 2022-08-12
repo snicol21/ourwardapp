d5e7f7de7c337200fece30415193948de6053a99
Brad Grow <brad.grow@utah.edu>
2022-08-12 13:54:04 -0600

    Removed semi-colon at the bottom of the site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <NextNProgress color="#4F46E5" height={5} />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   )
 }
