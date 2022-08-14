015d5805cb85d153390f57d6c62364eb62e68bfa
Spencer Nicol <Spencer.Nicol@gmail.com>
2022-08-13 20:59:55 -0600

    Update Footer.tsx

--- components/elements/footers/Footer.tsx
+++ components/elements/footers/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <Divider />
       <footer className="bg-gray-100">
         <div className="max-w-2xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-          <p className="text-center text-base text-gray-400">
+          <p className="text-center text-base text-gray-500">
             This is not an official website of The Church of Jesus Christ of Latter-day Saints.
             <br />
             It is operated by members of the {config.wardName}.
