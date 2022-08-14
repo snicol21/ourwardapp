01f33e62c41de4eafd834f6e855c636366983fd9
snicol21 <Spencer.Nicol@gmail.com>
2022-08-13 21:23:13 -0600

    add link for church website

--- components/elements/footers/Footer.tsx
+++ components/elements/footers/Footer.tsx
@@ -7,10 +7,19 @@ const Footer = () => {
       <Divider />
       <footer className="bg-gray-100">
         <div className="max-w-2xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-          <p className="text-center text-base text-gray-500">
-            This is not an official website of The Church of Jesus Christ of Latter-day Saints.
+          <p className="text-center text-sm text-gray-500">
+            This is <span className="underline font-semibold">not</span> an official website of{" "}
+            <a
+              href="https://www.churchofjesuschrist.org"
+              target="_blank"
+              className="whitespace-nowrap text-primary-500 hover:text-primary-900"
+              rel="noreferrer"
+            >
+              The Church of Jesus Christ of Latter-day Saints
+            </a>
+            .
             <br />
-            It is operated by members of the {config.wardName}.
+            It is operated by members of the <span className="whitespace-nowrap">{config.wardName}</span>.
           </p>
         </div>
       </footer>
