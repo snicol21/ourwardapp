2a4518122a963bf0737681ea89b23bdb92baee21
Brad Grow <brad.grow@utah.edu>
2023-04-17 14:17:20 -0600

    Fix properties.

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 export const config = {
   wardName: "Maples 6th Ward",
-  apiUrl: process.env.LATTER_DAY_API_URL,
-  apiWard: process.env.LATTER_DAY_API_WARD,
+  apiUrl: process.env.API_URL,
+  apiWard: process.env.API_WARD,
   wardAddress: {
     street: "8137 Echo View Dr",
     city: "West Jordan",
@@ -10,13 +10,13 @@ export const config = {
   },
   apiHeaders: {
     headers: {
-      "x-api-key": process.env.LATTER_DAY_API_TOKEN,
+      "x-api-key": process.env.API_TOKEN,
       "Content-Type": "application/json",
     },
   },
   pages: {
     index: {
-      heroCardId: process.env.LATTER_DAY_HERO_CARD,
+      heroCardId: process.env.API_HERO_CARD,
     },
   },
   httpHeaders: {
